0e4789fca27f0d270c549f1183b62e1cf4106a01
Ian Rust <iancharlesrust@gmail.com>
2013-11-29 15:42:15 -0800

    changed animation to be js based

--- js/behavior.js
+++ js/behavior.js
@@ -3,18 +3,53 @@ $(document).ready(function() {
     var ianEmail=parseEmail();
     ianEmail.set();
 
-    toFlip=$('#toflip')
+    var toFlip=$('#toflip')
+    var front=$('#front')
+    var back=$('#back')
 
-    $('.flipButton').unbind('click').click(function() {
-        console.log(toFlip.hasClass("flipped"))
+    var spinTime=500;
+
+    var applyAnimation = function(now,fx) {        
+      toFlip.css('-webkit-transform','rotateY('+now+'deg)');
+      toFlip.css('-moz-transform','rotateY('+now+'deg)'); 
+      toFlip.css('-ms-transform','rotateY('+now+'deg)');
+      toFlip.css('-o-transform','rotateY('+now+'deg)');
+      toFlip.css('transform','rotateY('+now+'deg)');
+    }
+
+    var finishAnimation=function() {
         if (toFlip.hasClass("flipped")) {
+            toFlip.animate({deg:0},
+                {
+                    step:applyAnimation,
+                    duration:spinTime
+                });
+            back.css('z-index',1);
+            front.css('z-index',2);
             toFlip.switchClass("flipped","notflipped");
             ianEmail.get();
         }
         else {
+            toFlip.animate({deg:180},
+                {
+                    step:applyAnimation,
+                    duration:spinTime
+                });
+            back.css('z-index',2);
+            front.css('z-index',1);
             toFlip.switchClass("notflipped","flipped");
             ianEmail.set();   
         }
+    }
+
+    $('.flipButton').unbind('click').click(function() {
+        console.log(toFlip.hasClass("flipped"))
+        toFlip.animate({deg:90},
+            {
+                step:applyAnimation,
+                complete:finishAnimation,
+                duration:spinTime
+            });
     });
 
 }
