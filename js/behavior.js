d4d16d9d6233303ea18549ffea9d87dfb4cadeab
Ian Rust <ianrust@google.com>
2013-11-29 14:22:14 -0800

    got email link working

--- js/behavior.js
+++ js/behavior.js
@@ -1,40 +1,50 @@
-$(document).ready(runBehavior); 
+$(document).ready(function() {
+    
+    var ianEmail=parseEmail();
+    ianEmail.set();
 
-$('#front').click(setEmail);
+    toFlip=$('#toflip')
 
-$('.flipper').click(flip);
+    $('.flipButton').unbind('click').click(function() {
+        console.log(toFlip.hasClass("flipped"))
+        if (toFlip.hasClass("flipped")) {
+            toFlip.switchClass("flipped","notflipped");
+            ianEmail.get();
+        }
+        else {
+            toFlip.switchClass("notflipped","flipped");
+            ianEmail.set();   
+        }
+    });
 
-function flip() {
-    if (this.id==="flipped") {
-        this.id="notflipped";
-    }
-    else {
-        this.id="flipped";        
+}
+); 
+
+
+
+function parseEmail() {
+
+    var requestStr = "http://randomword.setgetgo.com/get.php";
+    var email="wat";
+
+    function RandomWordComplete(data) {
+        var email="mailto:"+data.Word.slice(0,-2)+"@ianru.st";
+        return email
+    };
+
+    return  { 
+        set: function() {$.ajax({
+                type: "GET",
+                url: requestStr,
+                dataType: "jsonp",        
+                success: function (data) {
+                    email=RandomWordComplete(data);
+                }   
+            });
+        },
+        get: function() {
+            $('#emailLink').attr('href',email)
+        }
     }
 
 }
-
-function runBehavior() {
-    setEmail();
-};
-
-function setEmail() {
-
-
-    var requestStr = "http://randomword.setgetgo.com/get.php";
-    var email;
-    $.ajax({
-        type: "GET",
-        url: requestStr,
-        dataType: "jsonp",        
-        success: function (data) {
-            email=RandomWordComplete(data);
-            console.log(email);
-        }
-    });
-};
-
-function RandomWordComplete(data) {
-    var email="mailto:"+data.Word.replace(/(\r\n|\n|\r)/gm)+"@ianru.st";
-    return email
-};
